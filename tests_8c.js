3f13f47326bf9d6aa00637eb06f2978650d34844
tsaiyuyan <tsaiyuyan@users.noreply.github.com>
2021-11-02 08:31:35 +0000

    deploy: b74172cdf19e1c1792e91c92309505a5c9c8c48a

--- tests_8c.js
+++ tests_8c.js
@@ -3,6 +3,7 @@ var tests_8c =
     [ "HTTP_SERVICE_PORT", "tests_8c.html#ab86a7963a7c5b43be55ed24dced01f3f", null ],
     [ "HTTPS_SERVICE_PORT", "tests_8c.html#a916ebd29e271a9d7427d19b14f1778ce", null ],
     [ "ssl_perror", "tests_8c.html#a33abe210666ac23cd667458558e93a43", null ],
+    [ "global_ssl_init", "tests_8c.html#a3bdec78989621269befbf2063e4fb296", null ],
     [ "main", "tests_8c.html#aefc00cbac28210cf9a6d0050ca8ce2eb", null ],
     [ "simple_http_server", "tests_8c.html#a04ba502f3fb7746bc18b8a12f9c84003", null ],
     [ "simple_https_server", "tests_8c.html#a7c1479d79584da3c02da7c5475dede22", null ],
