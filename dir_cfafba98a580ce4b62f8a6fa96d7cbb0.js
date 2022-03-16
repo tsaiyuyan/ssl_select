d15398998e2231b47fe71090c58c1c814194d44e
tsaiyuyan <tsaiyuyan@users.noreply.github.com>
2022-03-16 08:59:17 +0000

    deploy: a9abd9724829a84abeb94d294d1bf7bca6e16643

---
+++ dir_cfafba98a580ce4b62f8a6fa96d7cbb0.js
@@ -0,0 +1,5 @@
+var dir_cfafba98a580ce4b62f8a6fa96d7cbb0 =
+[
+    [ "testc.c", "testc_8c.html", "testc_8c" ],
+    [ "tests.c", "tests_8c.html", "tests_8c" ]
+];
--- files_dup.js
+++ files_dup.js
@@ -1,7 +1,6 @@
 var files_dup =
 [
-    [ "testc.c", "testc_8c.html", "testc_8c" ],
-    [ "tests.c", "tests_8c.html", "tests_8c" ],
+    [ "example", "dir_cfafba98a580ce4b62f8a6fa96d7cbb0.html", "dir_cfafba98a580ce4b62f8a6fa96d7cbb0" ],
     [ "ssl_select.c", "ssl__select_8c.html", "ssl__select_8c" ],
     [ "ssl_select.h", "ssl__select_8h.html", "ssl__select_8h" ]
 ];
